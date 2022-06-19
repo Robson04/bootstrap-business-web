f07c1a36fa9f28ae47a8498cf03dc9c54b56c84e
Robert <50234174+Robson04@users.noreply.github.com>
2022-06-19 11:37:07 +0200

    Delete db-pattern.sql

--- db-pattern.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS `pictures` (
-  `uid` int(11) NOT NULL AUTO_INCREMENT,
-  `categoryname` tinytext DEFAULT NULL,
-  `path` text DEFAULT NULL,
-  `adddate` date NOT NULL DEFAULT curdate(),
-  PRIMARY KEY (`uid`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
